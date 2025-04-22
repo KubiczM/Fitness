fceb49ea20d2e03e3f0af1357e186aaccbee361a
Michał Kubicz <mqbicz@gmail.com>
2025-04-22 12:20:36 +0200

    attempt to fix code

--- operations/display_operations.py
+++ operations/display_operations.py
@@ -1,18 +1,18 @@
 def display_client_data(client_data):
 
-    print(f"\nClient: {client_data['first name']} {client_data['last name']}")
-    print(f"Body weight: {client_data['body weight [kg]']} kg")
-    print(f"Height: {client_data['height [cm]']} cm")
-    print(f"Age: {client_data['age [years]']} years")
-    print(f"Calories balance: {client_data['calories balance [cal]']} cal\n")
+    print(f"\nClient: {client_data['first_name']} {client_data['last_name']}")
+    print(f"Body weight: {client_data['body_weight']} kg")
+    print(f"Height: {client_data['height']} cm")
+    print(f"Age: {client_data['age']} years")
+    print(f"Calories balance: {client_data['calories_balance']} cal\n")
 
     print("Body Measurements:")
-    print(f"  Leg length: {client_data['legs']['legs [cm]']} cm")
-    print(f"  Tibia length: {client_data['legs']['tibia [cm]']} cm")
-    print(f"  Femur length: {client_data['legs']['femur [cm]']} cm")
-    print(f"  Wingspan: {client_data['upper limbs']['wingspan [cm]']} cm")
-    print(f"  Ulna: {client_data['upper limbs']['ulna [cm]']} cm")
-    print(f"  Humerus: {client_data['upper limbs']['humerus [cm]']} cm\n")
+    print(f"  Leg length: {client_data['leg_length']} cm")
+    print(f"  Tibia length: {client_data['tibia_length']} cm")
+    print(f"  Femur length: {client_data['femur_length']} cm")
+    print(f"  Wingspan: {client_data['wingspan']} cm")
+    print(f"  Ulna: {client_data['ulna']} cm")
+    print(f"  Humerus: {client_data['humerus']} cm\n")
 
     def print_strategy(title, strategy_dict):
         print(f"{title}")
